b3b5189a8eedc9bc1cd4c0340283226010e049f3
Mark Clarke <mark@jumpingbean.co.za>
2020-08-04 20:37:34 +0200

    Update __manifest__.py
    Bumped version in hope it will get flectra store to process module

--- payment_vcsweb/__manifest__.py
+++ payment_vcsweb/__manifest__.py
@@ -12,7 +12,7 @@
     'author': "Jumping Bean",
     'website': "http://www.jumpingbean.co.za",
     'category': 'Accounting',
-    'version': '1.0',
+    'version': '1.1',
     'license': 'LGPL-3'
 
     # any module necessary for this one to work correctly
